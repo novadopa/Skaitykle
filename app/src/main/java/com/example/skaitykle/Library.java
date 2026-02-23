5032cbc0cb782a96f2914c9ac2cbcb7f0ee1312a
Highwayman2004 <dainiuspetraitis24@gmail.com>
2026-02-23 17:22:04 +0200

    nauji pakeitimai meniu

--- app/src/main/java/com/example/skaitykle/Library.java
+++ app/src/main/java/com/example/skaitykle/Library.java
@@ -23,7 +23,7 @@ public class Library extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_library);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.library_main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
