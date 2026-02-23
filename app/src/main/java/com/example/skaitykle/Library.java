fd3d32117a9b41c6880563304b6a913dbe028da6
Highwayman2004 <dainiuspetraitis24@gmail.com>
2026-02-23 17:17:26 +0200

    Apacios meniu

--- app/src/main/java/com/example/skaitykle/Library.java
+++ app/src/main/java/com/example/skaitykle/Library.java
@@ -1,24 +1,60 @@
 package com.example.skaitykle;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class Library extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_library);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.library_main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                int id = menuItem.getItemId();
+
+                if(id == R.id.menu_home){
+                    Intent homeIntent = new Intent(getBaseContext(), Title.class);
+                    startActivity(homeIntent);
+                    return true;
+                }
+                else if (id == R.id.menu_library){
+                    Intent libraryIntent = new Intent(getBaseContext(), Library.class);
+                    startActivity(libraryIntent);
+                    return true;
+                }
+                else if(id == R.id.menu_profile){
+                    Intent profileIntent = new Intent(getBaseContext(), Profile.class);
+                    startActivity(profileIntent);
+                    return true;
+                }
+
+                return false;
+            }
         });
     }
 }
--- app/src/main/java/com/example/skaitykle/Title.java
+++ app/src/main/java/com/example/skaitykle/Title.java
@@ -15,7 +15,7 @@ public class Title extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_title);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.library_main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
